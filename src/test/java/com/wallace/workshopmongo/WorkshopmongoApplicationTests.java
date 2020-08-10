653599fcbc6f96593f42c883ddd617592ab9c2ab
wallacevncs@gmail.com <wallacevncs@gmail.com>
2020-08-10 12:07:32 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/wallace/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallace.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
