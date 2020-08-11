9e17fbb1f782509602f42016676db5d47a35e8a2
wallacevncs@gmail.com <wallacevncs@gmail.com>
2020-08-10 21:29:18 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/wallace/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.wallace.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wallace.workshopmongo.domain.User;
+import com.wallace.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
