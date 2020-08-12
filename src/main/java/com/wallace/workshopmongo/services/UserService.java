946a1d61975662da6425e9dc2f6b75ba152f4419
wallacevncs@gmail.com <wallacevncs@gmail.com>
2020-08-11 21:15:02 -0300

    Insercao de usuario com POST

--- src/main/java/com/wallace/workshopmongo/services/UserService.java
+++ src/main/java/com/wallace/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wallace.workshopmongo.domain.User;
+import com.wallace.workshopmongo.dto.UserDTO;
 import com.wallace.workshopmongo.repository.UserRepository;
 import com.wallace.workshopmongo.resources.exception.ObjectNotFoundException;
 
@@ -22,5 +23,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
