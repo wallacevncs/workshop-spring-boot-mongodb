479be77a0355648060a08ca64667d0633e1a36a5
wallacevncs@gmail.com <wallacevncs@gmail.com>
2020-08-12 11:52:30 -0300

    Obtendo um post por id

---
+++ src/main/java/com/wallace/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.wallace.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wallace.workshopmongo.domain.Post;
+import com.wallace.workshopmongo.repository.PostRepository;
+import com.wallace.workshopmongo.resources.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
