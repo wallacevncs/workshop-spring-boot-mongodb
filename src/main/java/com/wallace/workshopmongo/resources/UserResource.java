4b08a0e62421f9b1a8eb85304ae050e4a8906b7f
wallacevncs@gmail.com <wallacevncs@gmail.com>
2020-08-12 11:37:44 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/wallace/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wallace/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.wallace.workshopmongo.domain.Post;
 import com.wallace.workshopmongo.domain.User;
 import com.wallace.workshopmongo.dto.UserDTO;
 import com.wallace.workshopmongo.services.UserService;
@@ -59,4 +60,11 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
 }
