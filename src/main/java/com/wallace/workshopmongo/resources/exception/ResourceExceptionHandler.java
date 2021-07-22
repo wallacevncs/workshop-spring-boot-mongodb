51ece8f96b22dd04fefdca8ee0fabcdc3f04c498
wallacevncs <wallacevncs@gmail.com>
2021-07-22 11:36:08 -0300

    Images for readme added

--- src/main/java/com/wallace/workshopmongo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/wallace/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.wallace.workshopmongo.resources.exception.ObjectNotFoundException;
+
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
